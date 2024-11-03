0e347aa0968499b86f61946d4af3ee8beb283c09
Amit-admin <amit.ver02@gmail.com>
2024-11-04 02:13:17 +0530

    changed action added prob 6

--- src/main/scala/coding_set1_amitv.scala
+++ src/main/scala/coding_set1_amitv.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, current_date, datediff, initcap, lit, to_date, when}
+import org.apache.spark.sql.functions.{col, count, current_date, datediff, initcap, lit, sum, to_date, when}
+import org.apache.spark.sql.functions._
 
 object coding_set1_amitv {
   def main(args:Array[String]):Unit = {
@@ -55,32 +56,107 @@ object coding_set1_amitv {
       ("aadil", 32000)
     ).toDF("name", "total_sales")
 
-    val perfdf = sales.select(initcap(col("name")).alias("name"),
-      col("total_sales"),
-      when(col("total_sales")>50000,"Excellent")
-        .when(col("total_sales") between(25000,50000), "Good")
-        .otherwise("Need Improvement").alias("performanceStatus")
-    )
-    perfdf.show()
+//    val perfdf = sales.select(initcap(col("name")).alias("name"),
+//      col("total_sales"),
+//      when(col("total_sales")>50000,"Excellent")
+//        .when(col("total_sales") between(25000,50000), "Good")
+//        .otherwise("Need Improvement").alias("performanceStatus")
+//    )
+//    perfdf.show()
 
+//    3)
     val workload = List(
       ("karthik", "ProjectA", 120),
       ("karthik", "ProjectB", 100),
-    ("neha", "ProjectC", 80),
-    ("neha", "ProjectD", 30),
-    ("priya", "ProjectE", 110),
-    ("mohan", "ProjectF", 40),
-    ("ajay", "ProjectG", 70),
-    ("vijay", "ProjectH", 150),
-    ("veer", "ProjectI", 190),
-    ("aatish", "ProjectJ", 60),
-    ("animesh", "ProjectK", 95),
-    ("nishad", "ProjectL", 210),
-    ("varun", "ProjectM", 50),
-    ("aadil", "ProjectN", 90)
+      ("neha", "ProjectC", 80),
+      ("neha", "ProjectD", 30),
+      ("priya", "ProjectE", 110),
+      ("mohan", "ProjectF", 40),
+      ("ajay", "ProjectG", 70),
+      ("vijay", "ProjectH", 150),
+      ("veer", "ProjectI", 190),
+      ("aatish", "ProjectJ", 60),
+      ("animesh", "ProjectK", 95),
+      ("nishad", "ProjectL", 210),
+      ("varun", "ProjectM", 50),
+      ("aadil", "ProjectN", 90)
     ).toDF("name", "project", "hours")
 
+//    val wldf = workload.select( initcap(col("name")).alias("name"),col("project")
+//      ,col("hours"), when(col("hours")>200,"Overloaded")
+//                    .when(col("hours") between(100,200),"Balanced")
+//                    .otherwise("Underutilized").alias("category")
+//    )
+////    aggregated workload status count by category
+//    val finaldf = wldf.groupBy(col("category")).count()
+//    wldf.show()
+//    finaldf.show()
 
+//    4)
+    val employeesot = List(
+      ("karthik", 62),
+      ("neha", 50),
+      ("priya", 30),
+      ("mohan", 65),
+      ("ajay", 40),
+      ("vijay", 47),
+      ("veer", 55),
+      ("aatish", 30),
+      ("animesh", 75),
+      ("nishad", 60)
+    ).toDF("name", "hours_worked")
+
+//    val otdf = employeesot.select(initcap(col("name")).alias("name"), col("hours_worked")
+//      ,when(col("hours_worked") > 60, "Excessive Overtime")
+//        .when(col("hours_worked") between(45, 60), "Standard Overtime")
+//        .otherwise("No Overtime").alias("OvertimeStatus")
+//    ).groupBy(col("OvertimeStatus")).count()
+////      val otcntdf = otdf.groupBy(col("OvertimeStatus")).count()
+//    otdf.show()
+//    otcntdf.show()
+
+//    5) Customer age grouping
+        val customers = List(
+            ("karthik", 22),
+            ("neha", 28),
+            ("priya", 40),
+            ("mohan", 55),
+            ("ajay", 32),
+            ("vijay", 18),
+            ("veer", 47),
+            ("aatish", 38),
+            ("animesh", 60),
+            ("nishad", 25)
+        ).toDF("name", "age")
+
+    val custdf = customers.select(initcap(col("name")).alias("name"), col("age")
+      , when(col("age") < 25, "Youth")
+        .when(col("age") between(25, 45), "Adult")
+        .otherwise("Senior").alias("AgeStatus")
+    )
+    val custcntdf = custdf.groupBy(col("AgeStatus")).count()
+    custcntdf.show()
+    custdf.show()
+
+//    6) Vehicle Mileage
+    val vehicles = List(
+      ("CarA", 30),
+      ("CarB", 22),
+      ("CarC", 18),
+      ("CarD", 15),
+      ("CarE", 10),
+      ("CarF", 28),
+      ("CarG", 12),
+      ("CarH", 35),
+      ("CarI", 25),
+      ("CarJ", 16)
+    ).toDF("vehicle_name", "mileage")
+
+    val vehdf = vehicles.select(col("vehicle_name"), col("mileage")
+      , when(col("mileage") > 25, "High Efficiency")
+        .when(col("mileage") between(15, 25), "Moderate Efficiency")
+        .otherwise("Low Efficiency").alias("MileageStatus")
+    )
 
   }
 }
